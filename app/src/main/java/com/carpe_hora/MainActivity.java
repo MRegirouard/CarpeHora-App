979d4fa3a1efc306d35c311f0ed601f7c8faa751
Ethan Girouard <mr.egirouard@gmail.com>
2023-01-21 12:24:44 -0500

    Create Android app

---
+++ app/src/main/java/com/carpe_hora/MainActivity.java
@@ -0,0 +1,23 @@
+package com.carpe_hora;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.carpe_hora.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mTextView = binding.text;
+    }
+}
