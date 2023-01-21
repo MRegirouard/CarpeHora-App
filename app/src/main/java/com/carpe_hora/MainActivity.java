214fe770fd2cf2c2a961bf91da65aeeca2603643
Ethan Girouard <mr.egirouard@gmail.com>
2023-01-21 16:51:23 -0500

    Connect "Log now" button to log activity

--- app/src/main/java/com/carpe_hora/MainActivity.java
+++ app/src/main/java/com/carpe_hora/MainActivity.java
@@ -1,6 +1,9 @@
 package com.carpe_hora;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -17,5 +20,14 @@ public class MainActivity extends Activity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Button logBtn = (Button) findViewById(R.id.button2);
+
+        logBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this, LogActivity.class));
+                }
+        });
     }
 }
