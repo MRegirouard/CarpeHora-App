a8ef3f014c768d8758d31a28e75f6c5938277b31
ecco257 <connorjwittman@gmail.com>
2023-01-21 17:24:31 -0500

    make today list xml

---
+++ app/src/main/java/com/carpe_hora/TodayActivity.java
@@ -0,0 +1,14 @@
+package com.carpe_hora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Today extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_today);
+    }
+}
