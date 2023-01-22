648a619f8a88c5ee8101985b00d541c030eb98f3
Ethan Girouard <mr.egirouard@gmail.com>
2023-01-22 06:58:19 -0500

    Move login to Main

--- app/src/main/java/com/carpe_hora/MainActivity.java
+++ app/src/main/java/com/carpe_hora/MainActivity.java
@@ -6,13 +6,26 @@ import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.*;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.*;
+import com.google.firebase.firestore.*;
 
 import com.carpe_hora.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends Activity {
 
     private TextView mTextView;
     private ActivityMainBinding binding;
+    private GoogleApiClient googleApiClient;
+    public static String acctId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +52,45 @@ public class MainActivity extends Activity {
             }
         });
 
-        Button acctBtn = (Button) findViewById(R.id.acct_btn);
+        GoogleSignInOptions signInConfigBuilder = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
-        acctBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, signInConfigBuilder)
+                .build();
+
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, 0);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 0) {
+            GoogleSignInResult signInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (signInResult.isSuccess()) {
+                GoogleSignInAccount acct = signInResult.getSignInAccount();
+                acctId = acct.getId();
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                DocumentReference docRef = db.collection("users").document(acctId);
+
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(Task<DocumentSnapshot> documentSnapshot) {
+                        if (!documentSnapshot.isSuccessful() || !documentSnapshot.getResult().exists()) {
+                            Toast.makeText(getApplicationContext(), "Welcome new user " + acct.getGivenName(), Toast.LENGTH_SHORT).show();
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("days", new ArrayList<Object>());
+                            db.collection("users").document(acctId).set(user);
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(getApplicationContext(), "Sign in failed", Toast.LENGTH_SHORT).show();
             }
-        });
+        }
     }
 }
